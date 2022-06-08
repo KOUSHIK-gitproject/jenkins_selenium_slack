b5b6f105837c342d0b7da2660701ebdd8e507a0c
Koushik Senapati <98518529+KOUSHIK-gitproject@users.noreply.github.com>
2022-06-08 10:21:11 +0530

    Update login_page.py

--- lib_folder/login_page.py
+++ lib_folder/login_page.py
@@ -20,7 +20,7 @@ from pathlib import Path
 class Config:
     @classmethod
     def read_config(self, section, key):
-        with open(r'C:\Users\47790147\PycharmProjects\SSOApplication\lib_folder\config.yaml') as f:
+        with open(r'https://github.com/KOUSHIK-gitproject/jenkins_selenium_slack/tree/master/lib_folder/config.yaml') as f:
             data = yaml.load(f, Loader=yaml.FullLoader)
         return data[section][key]
 
